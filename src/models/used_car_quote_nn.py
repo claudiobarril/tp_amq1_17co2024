0e6809e8d54062e8d2d74fc6898fd290ec287f91
Christian Pisani <christian.tpg@gmail.com>
2024-10-19 00:02:41 -0300

    add nn to models

--- src/models/used_car_quote_nn.py
+++ src/models/used_car_quote_nn.py
@@ -1,18 +1,41 @@
-from torch import nn
+import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
-class UsedCarQuoteNN(nn.Module):
+class UsedCarQuoteNN(torch.nn.Module):
     def __init__(self, input_size):
         super().__init__()
-        self.layers = nn.Sequential(
-            nn.BatchNorm1d(input_size),
-            nn.Linear(input_size, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, 1)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(input_size, 128),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(128, 64),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(64, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 1)
         )
 
+    def fit(self, train_loader, test_loader, epochs, optimizer, loss_fn):
+        for epoch in range(epochs):
+            self.train()
+            running_loss = 0.0
+            for X_batch, y_batch in train_loader:
+                optimizer.zero_grad()
+                y_pred = self(X_batch)
+                loss = loss_fn(y_pred, y_batch)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+            self.eval()
+            with torch.no_grad():
+                val_loss = 0.0
+                for X_val, y_val in test_loader:
+                    y_val_pred = self(X_val)
+                    val_loss += loss_fn(y_val_pred, y_val).item()
+
     def forward(self, x):
-        x = self.layers(x)
-        return x
+        return self.layers(x)
+
